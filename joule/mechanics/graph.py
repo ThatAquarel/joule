2aa6738940b105c6cc66c6b6424b8492f0c96b8e
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-11-27 22:39:06 -0500

    speed up constant function

--- joule/mechanics/graph.py
+++ joule/mechanics/graph.py
@@ -37,12 +37,12 @@ class GraphEngine:
         samples = mesh.reshape((-1, 2))
 
         return samples
-    
+
     def _build_normals(self, f_xy, x, y, eval_mesh):
-        d_dx_mesh = self._d_ds_mesh(f_xy, x, y, x, eval_mesh) #  y constant, xz tan vec
+        d_dx_mesh = self._d_ds_mesh(f_xy, x, y, x, eval_mesh)  #  y constant, xz tan vec
         d_dx_vec = self._d_ds_vec(d_dx_mesh, 2, 0)
 
-        d_dy_mesh = self._d_ds_mesh(f_xy, x, y, y, eval_mesh) #  x constant, yz tan vec
+        d_dy_mesh = self._d_ds_mesh(f_xy, x, y, y, eval_mesh)  #  x constant, yz tan vec
         d_dy_vec = self._d_ds_vec(d_dy_mesh, 2, 1)
 
         normals = np.cross(d_dx_vec, d_dy_vec, axis=1)
@@ -54,11 +54,11 @@ class GraphEngine:
         f_d_ds = lambdify([x, y], d_ds, "numpy")
 
         z = f_d_ds(*eval_mesh.T)
-        if d_ds.is_constant():
+        # if d_ds.is_constant():
+        if len(eval_mesh) != len(z):
             return np.ones(len(eval_mesh)) * z
         return z
 
-    
     def _d_ds_vec(self, d_ds_mesh, opp_idx, adj_idx):
         vec = np.zeros((*d_ds_mesh.shape, 3), dtype=np.float32)
         vec[:, adj_idx] = 1
