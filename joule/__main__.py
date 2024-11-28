9eabb6a4d760e829629e03d6b67eb76304f788c1
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-11-27 22:38:52 -0500

    optional profile

--- joule/__main__.py
+++ joule/__main__.py
@@ -1,4 +1,11 @@
 from joule.app import run
 
+
 if __name__ == "__main__":
     run()
+
+    # import cProfile
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+    # profiler.disable()
+    # profiler.dump_stats("profile.prof")
