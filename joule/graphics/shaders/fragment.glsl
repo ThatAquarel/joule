4c9699e14c2fdf3175cf7f36f8eeb72d48e9b104
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-11-28 21:36:32 -0500

    refactor shaders

--- joule/graphics/shaders/fragment.glsl
+++ joule/graphics/shaders/fragment.glsl
@@ -1,35 +1,34 @@
 #version 330 core
 
 uniform vec3 view_pos;
-in vec3 light_pos;
+uniform vec3 light_pos;
+uniform vec3 light_color;
 
-in vec3 v_color;
-in vec3 v_normal;
-in vec3 v_frag_pos;
+uniform float ambient_strength;
+uniform float diffuse_strength;
+uniform float diffuse_base;
+uniform float specular_strength;
+uniform float specular_reflection;
+
+in vec3 vertex_color;
+in vec3 vertex_normal;
+in vec3 vertex_frag_pos;
 
 out vec4 fragColor;
 
 void main() {
-    vec3 light_color = vec3(1.0, 1.0, 1.0);
+    vec3 ambient = ambient_strength * light_color;
 
-    vec3 norm = normalize(v_normal);
+    vec3 light_dir = normalize(light_pos - vertex_frag_pos);  
+    float diffuse_component = max(abs(dot(vertex_normal, light_dir)), 0.0);
+    vec3 diffuse = sqrt(diffuse_base + diffuse_component * light_color * diffuse_strength);
 
-    float ambient_strength = 0.2;
-    vec3 ambient = ambient_strength * (light_color);
+    vec3 view_dir = normalize(view_pos - vertex_frag_pos);
+    vec3 reflect_dir = reflect(-light_dir, vertex_normal);
+    float ray_align = max(dot(view_dir, reflect_dir), 0.0);
+    float specular_component = pow(ray_align, specular_reflection);
+    vec3 specular = specular_strength * specular_component * light_color;  
 
-    float diffuseStrength = 0.8;
-    vec3 lightDir = normalize(light_pos - v_frag_pos);  
-    float diff = max(abs(dot(norm, lightDir)), 0.0);
-    vec3 diffuse_ = diff * light_color * diffuseStrength;
-    vec3 diffuse = 0.25 + diffuse_ * 0.25;
-
-    float specularStrength = 0.1;
-    vec3 viewDir = normalize(view_pos - v_frag_pos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * light_color;  
-
-    vec3 result = (ambient + sqrt(diffuse) + specular) * v_color;
-    fragColor = vec4(result, 1.0);
+    vec3 combined = (ambient + diffuse + specular) * vertex_color;
+    fragColor = vec4(combined, 1.0);
 }
--- joule/graphics/shaders/vertex.glsl
+++ joule/graphics/shaders/vertex.glsl
@@ -4,31 +4,19 @@ layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
 layout(location = 2) in vec3 normal;
 
+uniform mat4 world_transform;
 uniform mat4 cam_projection;
-uniform mat4 cam_position;
+uniform mat4 cam_transform;
 
-uniform vec4 light_pos_u;
-out vec3 light_pos;
-
-// out vec3 originalPosition;
-out vec3 v_color;
-out vec3 v_normal;
-out vec3 v_frag_pos;
+out vec3 vertex_color;
+out vec3 vertex_normal;
+out vec3 vertex_frag_pos;
 
 void main() {
-    mat4 rh = mat4(
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 0.0, 1.0
-    );
+    mat4 t = world_transform * cam_transform;
+    gl_Position = vec4(position, 1.0) * t * cam_projection;
 
-    gl_Position = vec4(position, 1.0) * rh * cam_position * cam_projection;
-
-    v_color = color;
-    v_normal = vec3(vec4(normal, 1.0) * rh);
-    v_frag_pos = vec3(vec4(position, 1.0) * rh);
-
-    light_pos = vec3(light_pos_u * inverse(cam_position));
-    // originalPosition = position;
+    vertex_color = color;
+    vertex_normal = vec3(vec4(normal, 1.0) * world_transform);
+    vertex_frag_pos = vec3(vec4(position, 1.0) * world_transform);
 }
