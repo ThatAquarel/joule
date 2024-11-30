8aca73c9c813555386b4a74aed193ec6ed1d7d6d
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-11-29 21:37:56 -0500

    refactor computation

--- joule/mechanics/graph.py
+++ joule/mechanics/graph.py
@@ -3,10 +3,8 @@ import sympy as sp
 
 from joule.graphics.elements.surface import Surface
 
-from joule.mechanics.parse import parse_function
 
-
-class GraphEngine:
+class MathEngine:
     def __init__(self, res=1024):
         self.surface = Surface(res=res)
 
@@ -29,18 +27,51 @@ class GraphEngine:
         d_ds_lambda = self._function_lambda(variables, d_ds)
 
         return d_ds_lambda
-    
+
     def get_f_xy(self):
         return self._f_xy
-    
+
     def get_d_dx(self):
         return self._d_dx
-    
+
     def get_d_dy(self):
         return self._d_dy
 
+    def _parse_function(self, equation):
+        x, y = sp.symbols("x y")
+
+        allowed = {
+            "x": x,
+            "y": y,
+            "sin": sp.sin,
+            "asin": sp.asin,
+            "cos": sp.cos,
+            "acos": sp.acos,
+            "sinh": sp.sinh,
+            "asinh": sp.asinh,
+            "cosh": sp.cosh,
+            "acosh": sp.acosh,
+            "tanh": sp.tanh,
+            "atanh": sp.atanh,
+            "pow": sp.Pow,
+            "sqrt": sp.sqrt,
+            "exp": sp.exp,
+            "ln": sp.ln,
+            "log": sp.log,
+            "ceil": sp.ceiling,
+            "floor": sp.floor,
+        }
+
+        f_xy = sp.sympify(
+            equation,
+            locals=allowed,
+            rational=True,
+        )
+
+        return (x, y), f_xy
+
     def update_function(self, equation):
-        x_y, f_xy = parse_function(equation)
+        x_y, f_xy = self._parse_function(equation)
 
         self._f_xy = self._function_lambda(x_y, f_xy)
         x, y = x_y
