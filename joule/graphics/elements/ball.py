a32cd364677ca580d12c9074cf117a7b0f5b8505
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2024-12-05 19:49:43 -0500

    render elementscomment

--- joule/graphics/elements/ball.py
+++ joule/graphics/elements/ball.py
@@ -9,62 +9,129 @@ from joule.graphics.vbo import create_vao, draw_vao, update_vbo
 
 
 def generate_sphere_vertices_fast(radius, res):
+    """
+    Generate vertices of a sphere for OpenGL draw
+
+    :param radius: radius of sphere
+    :param res: resolution of vertices
+    """
+
+    # preallocate an array to contain the vertices
+    # that takes the value of radius
     v = np.ones((res, res, 4, 3), dtype=np.float32) * radius
 
+    # compute the set of latitudes and longitudes
+    # of every point to form a sphere
     latitude = np.linspace(np.pi / 2, -np.pi / 2, res + 1)
     longitude = np.linspace(0, 2 * np.pi, res + 1)
 
+    # compute possible combinations of long and lat
+    # for every vertex that lies tangent to unit sphere
     long, lat = np.meshgrid(longitude, latitude)
 
+    # every four vertices form a quadrilaterial
+    # (ie: two triangles for OpenGL)
     for i, j in product(range(2), range(2)):
+        # makes sure that index rollsover properly
         endpoint = lambda n: n if n != 0 else None
         ii = endpoint(i - 1)
         jj = endpoint(j - 1)
 
+        # get current long and lat of quadrilateral
         l_i = long[i:ii, i:ii]
         l_j = lat[j:jj, j:jj]
 
+        # project spherical coordinates back into
+        # cartesian
+        # formulas from: https://en.wikipedia.org/wiki/Spherical_coordinate_system
         v[:, :, i + j * 2, 0] *= np.cos(l_j) * np.cos(l_i)
         v[:, :, i + j * 2, 1] *= np.cos(l_j) * np.sin(l_i)
         v[:, :, i + j * 2, 2] *= np.sin(l_j)
 
+    # return cartesian vertices of shape (n, 3)
     return v.reshape((-1, 3))
 
 
 class Ball:
     def __init__(self, initial_color, res=25):
+        """
+        Ball: Balls render element
+
+        :param initial_color: Balls initial color
+        :param res: Balls vertices resolution
+        """
+
+        # generate a unit sphere coordinates
         vertices = generate_sphere_vertices_fast(1, res)
         self.n = len(vertices)
+
+        # preallocate data buffer, containing
+        # position, color and normals
         self.data = np.ones((len(vertices), 9), dtype=np.float32)
-        # self.data[:, :3] = vertices * 0.125 + [1, 1, 1]
-        # self.data[:, :3] = vertices * 0.125
         self.data[:, :3] = vertices
         self.data[:, 3:6] = initial_color
         self.data[:, 6:9] = vertices
 
+        # build VAO and VBO for OpenGL
         self.vao, self.vbo = create_vao(self.data, return_vbo=True, store_normals=True)
 
     def _draw_ball(self, r, s):
+        """
+        Draw a ball of radius r at position s
+
+        :param r: Scalar radius of ball
+        :param s: Position of ball as vector of shape (3,)
+        """
+
         # TODO: Remove this redundant memcopy
+        # this is very slow, but it'll require a refactor of the
+        # whole rendering architecture, which I didn't have time to do
+
+        # make a copy of the default buffer
         data = np.copy(self.data)
+        # resize ball to new resize and translate
         data[:, :3] = data[:, :3] * r + s
 
+        # update VBO and draw this ball
         update_vbo(self.vbo, data)
         draw_vao(self.vao, GL_TRIANGLE_STRIP, self.n)
 
     def set_color(self, new_color):
+        """
+        Update all balls' color
+
+        :param new_color: New balls color
+        """
+
+        # change default buffer's color, so that subsequent
+        # copies in self._draw_ball take this new color
         self.data[:, 3:6] = new_color
         update_vbo(self.vbo, self.data)
 
     def draw(self, positions, masses, calculus_engine: CalculusEngine):
+        """
+        Draw all balls
+
+        :param positions: Array of positions to draw ball of shape (n, 3)
+        :param masses: Array of ball masses of shape (n,)
+        :param calculus_engine: joule.compute.CalculusEngine instance
+        """
+
+        # skip draw if no balls
         if not len(positions):
             return
 
+        # evaluate normals at the position of the balls
         point_mesh = positions[:, :2]
         normals = calculus_engine.build_normals(point_mesh)
 
+        # calculate radius based on uniform density
+        # V=4pi r^3/3
         radii = np.cbrt(3 * masses / (4 * np.pi)) * 0.08
 
+        # using the normals, place each ball tangential to the surface
         positions += normals * column_wise(radii)
+
+        # draw each ball with its distinct radius
         for pos, radius in zip(positions, radii):
             self._draw_ball(radius, pos)
--- joule/graphics/elements/surface.py
+++ joule/graphics/elements/surface.py
@@ -6,14 +6,24 @@ from joule.graphics.vbo import create_vao, draw_vao, update_vbo
 
 class Surface:
     def __init__(self, initial_color, res=1024):
+        """
+        Surface: Surface render element for function plotting
+
+        :param initial_color: Surface initial color
+        :param res: Evaluation points per axis (total points of res*res)
+        """
+
+        # prebuffer mesh and indices
         self._point_mesh = self._build_point_mesh(res)
         self._mesh_index = self._build_indices(res)
 
+        # preallocate data array with color
         self._n = len(self._mesh_index)
         self._data = np.ones((self._n, 9), dtype=np.float32)
 
         self._data[:, 3:6] = initial_color
 
+        # build VAO
         self._vao, self._vbo = create_vao(
             self._data,
             return_vbo=True,
@@ -23,18 +33,37 @@ class Surface:
         self.ready = False
 
     def _build_point_mesh(self, res):
+        """
+        Build unit grid of points with resolution
+
+        :param res: Resolution at which to sample
+        :return: Mesh of points of shape (n, 2)
+        """
+
+        # mesh of points
         mesh = np.mgrid[0 : 1 : res * 1j, 0 : 1 : res * 1j].T
         samples = mesh.reshape((-1, 2))
 
+        # return points of shape (n, 2)
         return samples
 
     def _build_indices(self, res):
+        """
+        Build draw indices for OpenGL triangle draw
+
+        :param res: Resolution at which to sample
+        :return: OpenGL triangle draw indices of shape (n,)
+        """
+
+        # allocate empty array for indices
+        # I can explain this part in person, it'll probably be easier
         idx = np.empty((res - 1, 3 * res - 2), dtype=np.int32)
         for i in range(2):
             i_iter = np.arange(i, res - 1 + i)
             j_iter = np.arange(res)
             idx[:, i : 2 * res : 2] = i_iter[:, np.newaxis] * res + j_iter
 
+        # numpy magic
         i_iter = np.arange(1, res)
         j_iter = np.arange(res - 2, 0, -1)
         idx[:, -(res - 2) :] = i_iter[:, np.newaxis] * res + j_iter
@@ -42,20 +71,52 @@ class Surface:
         return idx.flatten()
 
     def _point_mesh_scale(self, x_range, y_range):
+        """
+        Build a lambda function to scale a unit grid
+        to a range
+
+        :param x_range: New range of x values
+        :param y_range: New range of y values
+
+        :return: points scaling lambda
+        """
+
         ranges = [x_range, y_range]
 
+        # find the delta of the interval
         intervals = np.subtract.reduce(ranges, axis=1)
         intervals = np.abs(intervals)
+
+        # find the minimum of the interval
         minimums = np.min(ranges, axis=1)
 
         return lambda vec: vec * intervals + minimums
 
     def get_point_mesh(self, x_range, y_range):
+        """
+        Build a scaled grid of points
+
+        :param x_range: range of x values
+        :param y_range: range of y values
+        :return: Mesh of points of shape (n, 2)
+        """
+
+        # scale buffered unit point mesh to new range
         scale = self._point_mesh_scale(x_range, y_range)
         point_mesh = scale(self._point_mesh)
+
+        # return point mesh
         return point_mesh
 
     def set_color(self, new_color):
+        """
+        Update surface' color
+
+        :param new_color: New surface color
+        """
+
+        # change default buffer's color, so that subsequent
+        # updates in self.update_function take this new color
         self._data[:, 3:6] = new_color
         update_vbo(self._vbo, self._data)
 
@@ -65,6 +126,15 @@ class Surface:
         values,
         normals,
     ):
+        """
+        Update sampled points buffer
+
+        :param scaled_mesh: Mesh of points of shape (n, 2)
+        :param values: Array of f(x, y) function evaluations at points of shape (n,)
+        :param normals: Array of normals evaluted at points of shape (n, 3)
+        """
+
+        # copy new function data into buffers
         self._data[:, :2] = scaled_mesh[self._mesh_index]
         self._data[:, 2] = values[self._mesh_index]
         self._data[:, -3:] = normals[self._mesh_index]
@@ -73,6 +143,10 @@ class Surface:
         self.ready = True
 
     def draw(self):
+        """
+        Draw surface
+        """
+
         if not self.ready:
             return
 
